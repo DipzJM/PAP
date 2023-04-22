4c9bfaaa0d5a59e8544543a4ef7295161a678dc6
Your Name <30955@esjaloures.org>
2023-04-23 00:26:36 +0100

    Atualizado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,11 +16,30 @@ class UserController extends Controller
             $utilizadorVeiculo = new UtilizadorVeiculo;
             $utilizadorVeiculo->id_utilizador = $request->id_utilizador;
             $utilizadorVeiculo->id_veiculo = $request->id_veiculo;
-            $preco_veiculo = Veiculo::find($request->id_veiculo)->preco;
-        
+            $veiculo = Veiculo::find($request->id_veiculo);
+
+            if (!$veiculo) {
+                return response()->json([
+                    'status' => 'Erro',
+                    'message' => 'Veículo não encontrado.',
+                ]);
+            }
+            else 
+            {
+                $preco_veiculo = Veiculo::find($request->id_veiculo)->preco;
+            }
+
             $user = UserDetails::find($request->id_utilizador);
+            if (!$user) {
+                return response()->json([
+                    'status' => 'Erro',
+                    'message' => 'Utilizador não encontrado.',
+                ]);
+            }
             $saldoUser = $user->num_moedas;
-        
+
+            
+
             $utilizadorVeiculoExistente = UtilizadorVeiculo::where('id_utilizador', $request->id_utilizador)
                 ->where('id_veiculo', $request->id_veiculo)
                 ->first();
@@ -31,32 +50,32 @@ class UserController extends Controller
                     'message' => 'O utilizador já possui o veiculo requesitado.',
                 ]);
             }
-        
+
             if ($saldoUser < $preco_veiculo) {
                 return response()->json([
                     'status' => 'Erro',
                     'message' => 'Saldo insuficiente para comprar o veiculo requesitado.',
                 ]);
             }
-        
+
             $utilizadorVeiculo->save();
-        
+
 
             $user->num_moedas -= $preco_veiculo;
             $user->save();
-        
+
             return response()->json([
                 'status' => 'Sucesso',
                 'message' => 'Veiculo adquirido com sucesso.',
             ]);
-        
+
         } catch (Exception $e) {
             return response()->json([
                 'status' => 'Fracasso',
                 'message' => $e->getMessage(),
             ]);
         }
-        
+
     }
 
 }
