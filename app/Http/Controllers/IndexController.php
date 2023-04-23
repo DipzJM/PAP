f620960156a02bc648dfcbbcac0a566d712b8414
Your Name <30955@esjaloures.org>
2023-04-23 16:56:20 +0100

    atualizado

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,17 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use App\Models\Alert;
 use Illuminate\Http\Request;
 use App\Models\User;
+
 class IndexController extends Controller
 {
     public function indexPage()
     {
         $alert = Alert::where('ativo', 1)->first();
-        $user = User::with('UserDetails')->find(Auth::user()->id);
+
+        $user = null;
+
+        if (Auth::check()) {
+            $user = User::with('UserDetails')->find(Auth::user()->id);
+        }
+
         return view('index', isset($user) ? compact('user') : [], compact('alert'));
     }
+
 
 }
