ec024eb52642b5fea1155c040e74a45a946f4de8
Your Name <30955@esjaloures.org>
2023-04-23 13:25:18 +0100

    Fix user

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Models\Alert;
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class IndexController extends Controller
 {
     public function indexPage()
     {
         $alert = Alert::where('ativo', 1)->first();
-        return view('index', compact('alert'));
+        $user = User::with('UserDetails')->find(Auth::user()->id);
+        return view('index', isset($user) ? compact('user') : [], compact('alert'));
     }
 
 }
