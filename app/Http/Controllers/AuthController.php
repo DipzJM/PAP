939b7ea0524bc62523ff3a8c28ac4120fd89529d
JoaoMaia30955 <30955@esjaloures.org>
2023-05-06 16:56:03 +0100

    Mudança no tempo do token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -105,7 +105,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'expires_in' => null,
             'user' => Auth::guard('api')->user()
         ]);
     }
